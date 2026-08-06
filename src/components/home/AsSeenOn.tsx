2fc2c3f9adcdfe0a0ae555a79a00418415169b4a
Vivek <vivekr9164@gmail.com>
2026-08-06 19:55:13 +0530

    Double the size of the "As Seen On" logo marquee

--- src/components/home/AsSeenOn.tsx
+++ src/components/home/AsSeenOn.tsx
@@ -21,9 +21,9 @@ export default function AsSeenOn({ logos }: { logos: Logo[] }) {
                 <Image
                   src={logo.imageUrl}
                   alt={logo.name}
-                  width={115}
-                  height={30}
-                  className="h-[27px] w-auto object-contain opacity-70 grayscale"
+                  width={230}
+                  height={60}
+                  className="h-[54px] w-auto object-contain opacity-70 grayscale"
                   unoptimized
                 />
               ) : (
